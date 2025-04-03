12ff71bf1d5cf6417a4924491587ed2d2a6a941e
RHHEN122 <rokyhenderson222@gmail.com>
2025-04-03 16:07:05 +1300

    Add files via upload

---
+++ scripts/name.js
@@ -0,0 +1,8 @@
+document.title="Ricer-Tools"
+var link = document.querySelector("link[rel~='icon']");
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+}
+link.href = '../media/ricer-tools-favicon.png';
